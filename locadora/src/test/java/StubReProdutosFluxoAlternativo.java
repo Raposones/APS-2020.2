6f1ad042d4164b138d98f564a13fa4157967de38
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-08-19 22:08:05 -0300

    adicionado teste

--- locadora/src/test/java/StubReProdutosFluxoAlternativo.java
+++ locadora/src/test/java/StubReProdutosFluxoAlternativo.java
@@ -1,11 +1,16 @@
 import br.com.raposones.repositories.Rep_produtos;
+
+import java.util.HashMap;
+import java.util.List;
+
 import br.com.raposones.models.Produto;
+import br.com.raposones.models.VHS;
 
 public class StubReProdutosFluxoAlternativo extends Rep_produtos {
 
   @Override
   public Produto getProduto(String codigo) {
-    // Simula a ausência do produto
     return null;
   }
+
 }
--- locadora/src/test/java/StubReProdutosFluxoPrincipal.java
+++ locadora/src/test/java/StubReProdutosFluxoPrincipal.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import br.com.raposones.models.Produto;
 import br.com.raposones.models.VHS;
 import br.com.raposones.repositories.Rep_produtos;
@@ -5,14 +7,16 @@ import br.com.raposones.repositories.Rep_produtos;
 public class StubReProdutosFluxoPrincipal extends Rep_produtos {
   @Override
   public Produto getProduto(String codigo) {
-    // Simula um produto cadastrado com o código informado
     if ("0".equals(codigo)) {
-      // Cria um produto com informações fictícias
       Produto produto = new VHS("0", "Produto Exemplo", codigo, false, 2001, 103, false);
-      // Adicione outros atributos conforme necessário
       return produto;
     }
-    // Retorna null para códigos diferentes
+
     return null;
+  }
+
+  public List<Produto> getProdutos() {
+    return List.of(new VHS("0", "Produto 1", "Terror", false, 0, 0, false),
+        new VHS("1", "Produto 2", "Ação", false, 0, 0, false));
   }
 }
