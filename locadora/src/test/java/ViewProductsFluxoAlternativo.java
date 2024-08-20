f91245f09f368c039b13b5461fb17986a2a556bc
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-08-19 22:10:44 -0300

    adicionado teste

---
+++ locadora/src/test/java/ViewProductsFluxoAlternativo.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.raposones.models.Produto;
+
+public class ViewProductsFluxoAlternativo {
+  @Test
+  public void shouldReturnProductInfoWhenProductFound() {
+    StubReProdutosFluxoPrincipal rep_produtos = new StubReProdutosFluxoPrincipal();
+
+    List<Produto> produtos = rep_produtos.getProdutos();
+
+    assertNull(produtos);
+  }
+}
